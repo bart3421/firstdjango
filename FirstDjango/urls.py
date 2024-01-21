ee47838083d99d96b7cf8827b043a82623813028
Anastasiya Vagina <bart3421@gmail.com>
2024-01-21 18:24:17 +0300

    initial commit

---
+++ FirstDjango/urls.py
@@ -0,0 +1,9 @@
+from MainApp import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home),
+    path('about', views.about),
+    path('item/<int:item_id>', views.get_item),
+    path('items', views.get_items),
+]
